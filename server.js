589fb6baae74eacb53d60c1dcf19036ed6a9d6e6
Daeun Yun <daeun.yun@publy.co>
2023-01-09 23:23:39 +0900

    ✨ 정적 파일 내려주는 기능 추가

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("index.html"));
+app.use("/static", express.static(path.resolve(__dirname, "static")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "index.html"));
 });
 
 app.listen(3000, () => console.log("Server running..."));
