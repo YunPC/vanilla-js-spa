1204ee063f3f3da50d5766ed7c703dc22391d8d2
Daeun Yun <daeun.yun@publy.co>
2023-01-10 12:16:52 +0900

    ✨ 포스트 목록을 가져온 뒤 html로 변환

--- static/js/views/Home.js
+++ static/js/views/Home.js
@@ -6,11 +6,37 @@ export default class extends AbstractView {
     this.setTitle("신년 메시지 앱");
   }
 
+  async fetchMessages() {
+    const {
+      data: {
+        data: { posts },
+      },
+    } = await axios.get("http://43.201.103.199/posts");
+    return posts;
+  }
+
   async getHtml() {
+    const messages = await this.fetchMessages();
+    const postListHtml = messages
+      .map(
+        (
+          message
+        ) => `<li class="list-item"><a href="/${message.postId}" class="list-item-link">
+        <img src="${message.image}" alt="${message.title}의 첨부 이미지" class="card-image" />
+        <div class="list-description">
+          <span class="long-text">${message.title}</span>
+          <span class="long-text">${message.content}</span>
+        </div>
+      </a></li>`
+      )
+      .join("");
     return `
       <nav>
         <a href="/create" class="btn red" data-link>✏️ 새 글 작성하기</a>
       </nav>
+      <ul class="list-style">
+        ${postListHtml}
+      </ul>
     `;
   }
 }
