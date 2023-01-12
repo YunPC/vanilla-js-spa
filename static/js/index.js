5a3506b82cf72d399065f8dd3374a49a3449484a
Daeun Yun <daeun.yun@publy.co>
2023-01-12 10:56:16 +0900

    ✨ 신년 메시지 추가

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,21 @@
 import Create from "./views/Create.js";
 import Home from "./views/Home.js";
 
+const pathToRegex = (path) =>
+  new RegExp("^" + path.replace(/\//g, "\\/").replace(/:\w+/g, "(.+)") + "$");
+
 const navigateTo = (url) => {
   history.pushState(null, null, url);
   router();
 };
 
+let state = null;
+
 const router = async () => {
   const routes = [
     { path: "/", view: Home },
     { path: "/create", view: Create },
+    // { path: "/:id", view: Create },
   ];
 
   // Test each route for potential match
@@ -31,17 +37,39 @@ const router = async () => {
 
   const view = new match.route.view();
 
+  state = new match.route.view();
+
   document.querySelector("#app").innerHTML = await view.getHtml();
 };
 
 window.addEventListener("popstate", router);
 
 document.addEventListener("DOMContentLoaded", () => {
+  // event listener
   document.body.addEventListener("click", (e) => {
     if (e.target.matches("[data-link]")) {
       e.preventDefault();
       navigateTo(e.target.href);
     }
+    if (e.target.matches("[data-random-image-button]")) {
+      state.addRandomImage();
+      state.checkIsReadyToSubmit();
+    }
+    if (e.target.matches("[data-post-submit-button]")) {
+      state.postMessage();
+      navigateTo("/");
+    }
   });
+  document.body.addEventListener("input", (e) => {
+    if (e.target.matches("[data-title-input]")) {
+      state.title = e.target.value;
+      state.checkIsReadyToSubmit();
+    }
+    if (e.target.matches("[data-description-input]")) {
+      state.content = e.target.value;
+      state.checkIsReadyToSubmit();
+    }
+  });
+
   router();
 });
--- static/js/views/Create.js
+++ static/js/views/Create.js
@@ -3,12 +3,54 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
   constructor() {
     super();
-    this.setTitle("Create");
+    this.setTitle("신년 메시지 앱 - 작성 페이지");
+    this.imageUrl = "";
+    this.title = "";
+    this.content = "";
+  }
+
+  addBackButton() {
+    const header = document.querySelector("header");
+    header.innerHTML = `<a href="/">&#8249</a> HPNY 2023`;
+  }
+
+  async addRandomImage() {
+    const accessKey = "UeQ6L9qTHM782PHDtD9kE0pf9epVGolNPsTToi-7qRo";
+    const {
+      data: { urls: raw },
+    } = await axios.get(
+      `https://api.unsplash.com/photos/random?client_id=${accessKey}`
+    );
+    this.imageUrl = raw.regular;
+
+    document.querySelector("[data-random-image-button]").disabled = true;
+  }
+
+  checkIsReadyToSubmit() {
+    const submitButton = document.querySelector("[data-post-submit-button]");
+    if (!!(this.imageUrl && this.title && this.content)) {
+      submitButton.disabled = false;
+      return;
+    }
+
+    submitButton.disabled = true;
+  }
+
+  postMessage() {
+    axios.post("http://43.201.103.199/post", {
+      title: this.title,
+      content: this.content,
+      image: this.imageUrl,
+    });
   }
 
   async getHtml() {
+    this.addBackButton();
     return `
-      <h1>hello every one this is create page</h1>
+      <button class="btn red" data-random-image-button>랜덤 이미지 추가하기</button>
+      <input type="text" aria-label="신년 메시지 제목" data-title-input/> 
+      <textarea aria-label="신년 메시지 내용" data-description-input></textarea>
+      <button class="btn red" disabled data-post-submit-button>글 작성하기</button>
     `;
   }
 }
