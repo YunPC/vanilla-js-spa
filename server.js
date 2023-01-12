f56437ff9aea2e9e77a053b46ba6c43ca1f0ccd2
Daeun Yun <daeun.yun@publy.co>
2023-01-12 10:52:08 +0900

    🚨 노드 모듈 패키지를 정적 파일 형태로 제공

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const app = express();
 
 app.use("/static", express.static(path.resolve(__dirname, "static")));
 
+app.use(
+  "/node_modules",
+  express.static(path.resolve(__dirname, "node_modules"))
+);
+
 app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "index.html"));
 });
